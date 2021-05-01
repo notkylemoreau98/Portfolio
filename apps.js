2b1620bf53232a6e1fb5ed169c422fe64928ef76
Kyle Moreau <williamkylemoreau@gmail.com>
2021-04-30 22:32:12 -0500

    'Mobile Fixes and Design Improvements'

---
+++ apps.js
@@ -0,0 +1,14 @@
+const mobileNav = document.querySelector('.home__navMobile');
+const burgerMenu = document.querySelector('.home__navBars');
+const mobileLinks = document.querySelectorAll('.nav__mobileLink')
+
+function openNav(e) {
+	mobileNav.style.transform = "translateX(0%)";
+}
+
+function closeNav(e) {
+	mobileNav.style.transform = "translateX(100%)";
+}
+
+burgerMenu.addEventListener('click', openNav);
+mobileLinks.forEach(mobileLink => mobileLink.addEventListener('click', closeNav));
